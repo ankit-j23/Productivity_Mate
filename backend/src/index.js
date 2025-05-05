b3203ec1bde4cca9ba3fd4ed0825752b7f358ada
Ankit <ank558899@gmail.com>
2025-05-05 23:08:26 +0530

    path error solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use('/api/todos' , todosRoutes)
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
-    app.get("*" , ( req,res ) => {
+    app.get("/*" , ( req,res ) => {
         res.sendFile(path.join(__dirname , "../front-end" , "dist" , "index.html"))
     })
 }
